d64d666c50119dbec3453869051583c265d42794
Chad Ellison <chad.ellison0123@gmail.com>
2018-05-25 00:05:07 -0500

    refactor serializer

--- app/serializers/piece_serializer.rb
+++ app/serializers/piece_serializer.rb
@@ -2,18 +2,13 @@ class PieceSerializer
   class << self
     def serialize(piece)
       {
-        data: {
-          id: piece.id,
-          type: 'piece',
-          attributes: {
-            position: piece.position,
-            positionIndex: piece.position_index,
-            color: piece.color,
-            pieceType: piece.piece_type,
-            movedTwo: piece.moved_two,
-            hasMoved: piece.has_moved,
-          }
-        }
+        id: piece.id,
+        position: piece.position,
+        positionIndex: piece.position_index,
+        color: piece.color,
+        pieceType: piece.piece_type,
+        movedTwo: piece.moved_two,
+        hasMoved: piece.has_moved
       }
     end
   end
