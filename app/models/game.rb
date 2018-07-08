c8bf31bf2b5d3855d6928ac34789b5738ed1efb3
Chad Ellison <chad.ellison0123@gmail.com>
2018-07-08 09:00:13 -0500

    wip

--- app/models/game.rb
+++ app/models/game.rb
@@ -92,32 +92,19 @@ class Game < ApplicationRecord
       signature = moves.order(:move_count).last.setup.position_signature
       move_indices = moves.map { |move| position_index_from_move(move.value).to_i }
 
-      # pawn_moved_two = false
-      # last_move = moves.order(:move_count).last
-      # last_move_position_index = position_index_from_move(last_move.value).to_i
-      # last_moved_piece_type = piece_type_from_position_index(last_move_position_index)
-
-      # need to handle very first move ...
-      # previous_location_of_last_moved_piece = moves[-2].setup.position_signature.split('.').detect do |value|
-      #   position_index_from_move(value).to_i == last_move_position_index
-      # end[-2..-1]
-      #
-      # if previous_location_of_last_moved_piece[1].to_i.abs - last_move.position[1].to_i.abs == 2 &&
-      #   last_moved_piece_type == 'pawn'
-      #     pawn_moved_two = true
-      # end
-
+      pawn_moved_two = pawn_moved_two?
+      last_move = moves.order(:move_count).last
       @pieces = signature.split('.').map do |piece_value|
         position_index = position_index_from_move(piece_value).to_i
-
+        moved_two = (pawn_moved_two && position_index == last_move_index(last_move)) ? true : false
         Piece.new({
           game_id: id,
           position: piece_value[-2..-1],
           position_index: position_index,
           color: color_from_position_index(position_index),
           piece_type: piece_type_from_position_index(position_index),
-          has_moved: move_indices.include?(position_index)
-          # moved_two: (pawn_moved_two && last_move_position_index == position_index)
+          has_moved: move_indices.include?(position_index),
+          moved_two: moved_two
         })
       end
     else
@@ -130,7 +117,7 @@ class Game < ApplicationRecord
   end
 
   def piece_type_from_position_index(position_index)
-    # this should handle promoted pawns
+    # this should handle promoted pawns lool at moves for promoted value
     return 'king' if [5, 29].include?(position_index)
     return 'queen' if [4, 28].include?(position_index)
     return 'bishop' if [3, 6, 27, 30].include?(position_index)
@@ -145,5 +132,28 @@ class Game < ApplicationRecord
 
   def find_piece_by_index(index)
     pieces.detect { |piece| piece.position_index == index }
+  end
+
+  def last_move_index(last_move)
+    position_index_from_move(last_move.value).to_i
+  end
+
+  def pawn_moved_two?
+    last_move = moves.order(:move_count).last
+    last_moved_piece_type = piece_type_from_position_index(last_move_index(last_move))
+    return false unless last_moved_piece_type == 'pawn'
+
+    if moves.count == 1
+      ['4', '5'].include?(last_move.value[-1])
+      true
+    else
+      previous_position = moves[-2].setup.position_signature.split('.').detect do |value|
+        position_index_from_move(value).to_i == last_move_index(last_move)
+      end[-2..-1]
+
+      if previous_position[1].to_i.abs - last_move.value[-1].to_i.abs == 2
+        pawn_moved_two = true
+      end
+    end
   end
 end
