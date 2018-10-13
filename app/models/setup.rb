6d5b95393b49f1faf9e7865c5afbefe706572588
Chad Ellison <chad.ellison0123@gmail.com>
2018-10-13 12:11:01 -0500

    Do not look up signature if it already exists

--- app/models/setup.rb
+++ app/models/setup.rb
@@ -5,16 +5,18 @@ class Setup < ApplicationRecord
   belongs_to :material_signature, optional: true
   belongs_to :threat_signature, optional: true
 
-  SIGNATURE_CLASSES = [
-    AttackSignature, ThreatSignature, MaterialSignature, GeneralAttackSignature
-  ]
+  SIGNATURE_CLASSES = {
+    attack_signature: AttackSignature,
+    threat_signature: ThreatSignature,
+    material_signature: MaterialSignature,
+    general_attack_signature: GeneralAttackSignature
+  }
 
   def add_signatures(new_pieces, game_turn_code)
-    SIGNATURE_CLASSES.each do |signature_class|
-      signature = signature_class.create_signature(new_pieces, game_turn_code)
-
-      if signature.present?
-        signature_class.find_or_create_by(signature: signature).setups << self
+    SIGNATURE_CLASSES.each do |signature_key, signature_class|
+      if send(signature_key).blank?
+        signature = signature_class.create_signature(new_pieces, game_turn_code)
+        signature_class.find_or_create_by(signature: signature).setups << self if signature.present?
       end
     end
   end
