3f2a2680c4ad822a5f65937874525d4e6f163642
Chad Ellison <chad.ellison0123@gmail.com>
2019-02-16 09:27:00 -0600

    refactor

--- app/models/activity_signature.rb
+++ app/models/activity_signature.rb
@@ -2,7 +2,11 @@ class ActivitySignature
   def self.create_signature(new_pieces)
     signature = {}
     new_pieces.each do |piece|
-      signature[piece.position_index] = piece.valid_moves(new_pieces).count
+      if signature[piece.find_piece_code].present?
+        signature[piece.find_piece_code] += piece.valid_moves(new_pieces).count
+      else
+        signature[piece.find_piece_code] = piece.valid_moves(new_pieces).count
+      end
     end
 
     signature.map do |index, count|
