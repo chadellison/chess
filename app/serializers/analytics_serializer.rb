a329cdd415aed815f098645c0f567f0085a1cef3
Chad Ellison <chad.ellison0123@gmail.com>
2019-03-07 20:33:18 -0600

    wip

--- app/serializers/analytics_serializer.rb
+++ app/serializers/analytics_serializer.rb
@@ -1,16 +1,10 @@
 class AnalyticsSerializer
   class << self
-    def serialize(setup)
-      setup_outcomes = {}
-      setup_outcomes = setup.outcomes if setup.present?
+    def serialize(attributes)
       {
         data: {
           type: 'analytics',
-          attributes: {
-            whiteWins: setup_outcomes[:white_wins].to_i,
-            blackWins: setup_outcomes[:black_wins].to_i,
-            draws: setup_outcomes[:draws].to_i
-          }
+          attributes: attributes
         }
       }
     end
