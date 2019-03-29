6364bbab1d9ef20a0bd7630b949d6ce88f436c97
Chad Ellison <chad.ellison0123@gmail.com>
2019-03-28 21:39:52 -0500

    begin neural net

--- app/models/material_logic.rb
+++ app/models/material_logic.rb
@@ -1,32 +1,13 @@
 class MaterialLogic
-  MATERIAL_VALUE = { 1 => 5, 2 => 3, 3 => 3, 4 => 9, 5 => 0, 6 => 3, 7 => 3, 8 => 5,
-    9 => 1, 10 => 1, 11 => 1, 12 => 1, 13 => 1, 14 => 1, 15 => 1, 16 => 1, 17 => 1,
-    18 => 1, 19 => 1, 20 => 1, 21 => 1, 22 => 1, 23 => 1, 24 => 1, 25 => 5, 26 => 3,
-    27 => 3, 28 => 9, 29 => 0, 30 => 3, 31 => 3, 32 => 5
-  }
 
-  def self.find_value(index)
-    MATERIAL_VALUE[index]
-  end
+  MATERIAL_VALUE = { pawn: 1, knight: 3, bishop: 3, rook: 5, queen: 9, king: 0 }
 
   def self.create_signature(new_pieces, game_turn_code)
-    enemy_attackers = new_pieces.select do |piece|
-      piece.color[0] == game_turn_code && piece.enemy_targets.present?
-    end
-
-    enemy_attack_value = 0
-    enemy_attackers.each do |piece|
-      value = find_value(piece.enemy_targets.max_by { |target| find_value(target) })
-      enemy_attack_value = value if value > enemy_attack_value
-    end
-
-    enemy_attack_value *= -1 if game_turn_code == 'b'
-
-    material_value = new_pieces.reduce(enemy_attack_value) do |total, piece|
+    new_pieces.reduce(0) do |total, piece|
       if piece.color == 'black'
-        total - find_value(piece.position_index)
+        total - MATERIAL_VALUE[piece.piece_type.to_sym]
       else
-        total + find_value(piece.position_index)
+        total + MATERIAL_VALUE[piece.piece_type.to_sym]
       end
     end.to_s + game_turn_code
   end
--- app/models/neural_network.rb
+++ app/models/neural_network.rb
@@ -2,27 +2,15 @@ class NeuralNetwork
   def move_analysis(possible_moves, game_turn)
     weighted_moves = {}
 
-    possible_moves.shuffle.each do |possible_move|
+    possible_moves.each do |possible_move|
       setup = possible_move.setup
-      total_weight = setup.signatures.reduce(setup.average_outcome) do |weight, signature|
-        log_move_data(game_turn, signature, possible_move)
-        weight + signature.average_outcome
-      end
+      signatures = setup.signatures
+      total_weight = signatures.reduce(0) { |weight, signature| weight + signature.average_outcome }
 
       total_weight *= -1 if game_turn == 'black'
       weighted_moves[possible_move.value] = total_weight
+      puts "#{possible_move.value} ==> #{weighted_moves[possible_move.value]}"
     end
-    find_best_move(weighted_moves)
-  end
-
-  def log_move_data(game_turn, signature, possible_move)
-    puts "TURN: #{game_turn}"
-    puts "SIGNATURE TYPE #{signature.signature_type}"
-    puts "SIGNATURE: #{signature.value}"
-    puts "WEIGHT: #{possible_move.value} #{signature.rank.to_s}"
-  end
-
-  def find_best_move(weighted_moves)
-    weighted_moves.max_by { |move_value, weight| weight }.first
+    weighted_moves
   end
 end
