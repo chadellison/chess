1061e34cf8f8cc6730be84863b0841e5b9734574
Chad Ellison <chad.ellison0123@gmail.com>
2019-03-31 10:33:14 -0500

    tweak back propagation and refactor

--- app/models/neural_network.rb
+++ app/models/neural_network.rb
@@ -1,5 +1,5 @@
 class NeuralNetwork
-  ALPHA = 0.01
+  ALPHA = 0.05
 
   def move_analysis(possible_moves, game_turn)
     weighted_moves = {}
@@ -7,11 +7,14 @@ class NeuralNetwork
     possible_moves.each do |possible_move|
       signatures = ordered_signatures(possible_move.setup.signatures)
       weights = Weight.where(weight_type: signatures.pluck(:signature_type))
+      white_weights = filter_and_sort(weights, 'white')
+      black_weights = filter_and_sort(weights, 'black')
+      draw_weights = filter_and_sort(weights, 'draw')
 
       predictions = {
-        white: calculate_prediction(signatures, weights, 'white'),
-        black: calculate_prediction(signatures, weights, 'black'),
-        draw: calculate_prediction(signatures, weights, 'draw')
+        white: weighted_sum(signatures, white_weights),
+        black: weighted_sum(signatures, black_weights),
+        draw: weighted_sum(signatures, draw_weights)
       }
       weighted_moves[possible_move.value] = predictions
       puts "#{possible_move.value} ==> #{weighted_moves[possible_move.value]}"
@@ -29,12 +32,13 @@ class NeuralNetwork
 
       win_values.each do |win_value|
         sorted_weights = filter_and_sort(weights, win_value)
+        prediction = weighted_sum(signatures, sorted_weights)
 
         signatures.each_with_index do |signature, index|
           weight = sorted_weights[index]
 
           adjusted_weight_value = adjust_weight(
-            signature.average_outcome.to_f, weight.value.to_f, outcome
+            signature.average_outcome.to_f, weight.value.to_f, outcome, prediction
           )
           puts 'old weight: ' + weight.value
           weight.value = adjusted_weight_value.to_s
@@ -46,17 +50,11 @@ class NeuralNetwork
     weights_to_be_updated.each(&:save)
   end
 
-  def adjust_weight(input, weight, outcome)
-    prediction = input * weight
+  def adjust_weight(input, weight, outcome, prediction)
     puts 'PREDICTION: ' + (input * weight).to_s
     puts 'OUTCOME: ' + outcome.to_s
     puts 'ERROR: ' + (((input * weight) - outcome) ** 2).to_s
     weight - (ALPHA * derivative(input, prediction, outcome))
-  end
-
-  def calculate_prediction(signatures, weights, win_value)
-    sorted_weights = filter_and_sort(weights, win_value)
-    weighted_sum(signatures, sorted_weights)
   end
 
   def weighted_sum(signatures, weights)
