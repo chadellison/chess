a166c2c0dd95b102c3943dc724e069458e168e90
Chad Ellison <chad.ellison0123@gmail.com>
2019-04-02 22:58:29 -0500

    update attack signature

--- app/models/attack_logic.rb
+++ app/models/attack_logic.rb
@@ -1,11 +1,27 @@
 class AttackLogic
+
+  VALUE_FROM_INDEX = {
+    1 => 5, 2 => 3, 3 => 3, 4 => 9, 5 => 10, 6 => 3, 7 => 3, 8 => 5, 9 => 1,
+    10 => 1, 11 => 1, 12 => 1, 13 => 1, 14 => 1, 15 => 1, 16 => 1, 17 => 1,
+    18 => 1, 19 => 1, 20 => 1, 21 => 1, 22 => 1, 23 => 1, 24 => 1, 25 => 5,
+    26 => 3, 27 => 3, 28 => 9, 29 => 10, 30 => 3, 31 => 3, 32 => 5
+  }
+
   def self.create_signature(new_pieces, game_turn_code)
-    new_pieces.select { |piece| piece.enemy_targets.present? }.map do |piece|
-      piece.find_piece_code + 'x' + piece.enemy_targets.map do |enemy_target|
-        enemy_target.to_s + 'o' + Piece.defenders(enemy_target, new_pieces).map do |defender|
-          defender.find_piece_code
-        end.join('.')
-      end.join
-    end.join + game_turn_code
+    pieces_with_targets = new_pieces.select { |piece| piece.enemy_targets.present? }
+
+    find_target_by_color(pieces_with_targets, 'white') + 'x' +
+      find_target_by_color(pieces_with_targets, 'black') + game_turn_code
+  end
+
+  def self.find_target_by_color(pieces_with_targets, color)
+    target = ''
+    max_value = 0
+
+    pieces_with_targets.select { |piece| piece.color == color }.each do |piece|
+      target_index = piece.enemy_targets.max_by { |index| VALUE_FROM_INDEX[index] }
+      target = target_index if VALUE_FROM_INDEX[target_index] > max_value
+    end
+    target.to_s
   end
 end
--- app/models/material_logic.rb
+++ app/models/material_logic.rb
@@ -1,13 +1,10 @@
 class MaterialLogic
-
-  MATERIAL_VALUE = { pawn: 1, knight: 3, bishop: 3, rook: 5, queen: 9, king: 0 }
-
   def self.create_signature(new_pieces, game_turn_code)
     new_pieces.reduce(0) do |total, piece|
       if piece.color == 'black'
-        total - MATERIAL_VALUE[piece.piece_type.to_sym]
+        total - piece.find_piece_value
       else
-        total + MATERIAL_VALUE[piece.piece_type.to_sym]
+        total + piece.find_piece_value
       end
     end.to_s + game_turn_code
   end
