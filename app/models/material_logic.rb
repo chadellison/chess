6d97ee3a9056fa8acc5ee1eb6fb60d2884bd83d6
Chad Ellison <chad.ellison0123@gmail.com>
2019-04-19 21:37:49 -0500

    refactor signature methods

--- app/models/material_logic.rb
+++ app/models/material_logic.rb
@@ -1,20 +1,13 @@
 class MaterialLogic
-  INITIAL_VALUE = 0.01
-
   def self.create_signature(new_pieces)
-    white_material = INITIAL_VALUE
-    black_material = INITIAL_VALUE
-
-    new_pieces.each do |piece|
+    new_pieces.reduce(0) do |total, piece|
       piece_value = piece.find_piece_value
 
       if piece.color == 'white'
-        white_material += piece_value
+        total + piece_value
       else
-        black_material += piece_value
+        total - piece_value
       end
     end
-
-    (white_material / black_material).to_s
   end
 end
