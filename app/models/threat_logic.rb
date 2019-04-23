19bfbe9e897ef9a33e29c5a2be75b5e6c437d6ef
Chad Ellison <chad.ellison0123@gmail.com>
2019-04-23 17:48:29 -0500

    refactor and fix signature bug

--- app/models/threat_logic.rb
+++ app/models/threat_logic.rb
@@ -8,7 +8,7 @@ class ThreatLogic
       black_king = piece if piece.position_index == 5
       white_king = piece if piece.position_index == 29
       king_indices.include?(piece.position_index)
-    end.map(&:spaces_near_king)
+    end.map(&:spaces_near_king).flatten
 
     ControlledSpaceLogic.controlled_ratio(king_spaces, new_pieces)
       .to_s + game_turn_code
