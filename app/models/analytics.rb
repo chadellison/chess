a5010b0fba66507ee73b99fa84600810ab693a7e
Chad Ellison <chad.ellison0123@gmail.com>
2019-05-06 22:31:22 -0500

    cache analytics chart data

--- app/models/analytics.rb
+++ app/models/analytics.rb
@@ -1,28 +1,32 @@
 class Analytics
-  attr_reader :setup, :neural_network
+  include CacheLogic
 
-  def initialize(setup)
-    @setup = setup
+  def neural_network
     @neural_network = NeuralNetwork.new
   end
 
-  def move_analytics(moves)
-    game = Game.new
-    game.moves = moves.map { |attributes| Move.new(attributes) }
-
-    if game.moves.blank?
-      game.pieces
+  def move_analytics(analytics_params)
+    find_setup(analytics_params[:pieces], analytics_params[:turn])
+    if in_cache?(@setup.position_signature)
+      JSON.parse(get_from_cache(@setup.position_signature))
     else
-      game.last_move.setup = setup
-    end
+      game = load_game(analytics_params[:moves])
 
-    turn = game.moves.size.even? ? 'white' : 'black'
-    ai = AiLogic.new(game)
-    possible_moves = ai.find_next_moves(turn)
-    analyzed_moves = neural_network.move_analysis(possible_moves, turn).map do |next_move, prediction|
+      turn = game.moves.size.even? ? 'white' : 'black'
+      ai = AiLogic.new(game)
+      possible_moves = ai.find_next_moves(turn)
+      analyzed_moves = analyzed_moves(possible_moves, turn)
+
+      serialized_moves = AnalyticsSerializer.serialize(analyzed_moves)
+      add_to_cache(@setup.position_signature, serialized_moves)
+      serialized_moves
+    end
+  end
+
+  def analyzed_moves(possible_moves, turn)
+    neural_network.move_analysis(possible_moves, turn).map do |next_move, prediction|
       { move: next_move, white: prediction, black: prediction * -1 }
     end
-    AnalyticsSerializer.serialize(analyzed_moves)
   end
 
   def find_weight(signatures, signature_type)
@@ -38,5 +42,42 @@ class Analytics
     black_wins = Game.where(outcome: BLACK_WINS).count
     draws = Game.where(outcome: DRAW).count
     { white_wins: white_wins, black_wins: black_wins, draws: draws }
+  end
+
+  def find_setup(pieces, turn_code)
+    return @setup if @setup.present?
+
+    formatted_pieces = dersialize_pieces(pieces)
+    @setup = Setup.find_setup(formatted_pieces, turn_code)
+    @setup
+  end
+
+  def dersialize_pieces(pieces)
+    pieces.map do |piece|
+      Piece.new(
+        position_index: piece[:positionIndex],
+        piece_type: piece[:pieceType],
+        color: piece[:color],
+        position: piece[:position],
+        moved_two: piece[:movedTwo],
+        has_moved: piece[:hasMoved]
+      )
+    end
+  end
+
+  def dersialize_moves(moves)
+    moves.map { |attributes| Move.new(attributes) }
+  end
+
+  def load_game(moves)
+    game = Game.new
+    game.moves = dersialize_moves(moves)
+
+    if game.moves.blank?
+      game.pieces
+    else
+      game.last_move.setup = @setup
+    end
+    game
   end
 end
