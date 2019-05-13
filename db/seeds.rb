672baaa307a039be4035241542ab693a4ca27476
Chad Ellison <chad.ellison0123@gmail.com>
2019-05-13 11:43:06 -0500

    remove controlled space logic and threat logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ class Seeds
   def create_weights
     Weight.destroy_all
 
-    57.times { |count| Weight.create(weight_count: count + 1, value: rand) }
+    45.times { |count| Weight.create(weight_count: count + 1, value: rand) }
     puts 'created weights'
   end
 end
