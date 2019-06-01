f24fb72e0c56c358c1e6ecab302dd784d8c4e3b3
Chad Ellison <chad.ellison0123@gmail.com>
2019-05-31 20:10:20 -0500

    adjust the way outcomes are calculated

--- app/models/concerns/outcome_calculator.rb
+++ app/models/concerns/outcome_calculator.rb
@@ -2,16 +2,7 @@ module OutcomeCalculator
   extend ActiveSupport::Concern
 
   def outcome_ratio
-    return 0 if rank == 0
-    rank / total_played
-  end
-
-  def rank
-    outcomes[:white_wins].to_f - outcomes[:black_wins].to_f
-  end
-
-  def total_played
-    outcomes[:white_wins].to_f + outcomes[:black_wins].to_f + outcomes[:draws].to_f
+    outcomes[:white_wins].to_f - outcomes[:black_wins].to_f / 2
   end
 
   def update_outcomes(outcome)
--- app/models/threat_logic.rb
+++ app/models/threat_logic.rb
@@ -1,9 +1,12 @@
 class ThreatLogic
   def self.create_signature(new_pieces)
+    spaces_near_black_king = king_spaces(new_pieces, 5)
+    spaces_near_white_king = king_spaces(new_pieces, 29)
+
     new_pieces.reduce(0) do |sum, piece|
-      if can_check_without_being_attacked(5, piece, new_pieces)
+      if can_threaten?(spaces_near_black_king, piece, new_pieces)
         sum + 1
-      elsif can_check_without_being_attacked(29, piece, new_pieces)
+      elsif can_threaten?(spaces_near_white_king, piece, new_pieces)
         sum - 1
       else
         sum
@@ -11,8 +14,14 @@ class ThreatLogic
     end
   end
 
-  def self.can_check_without_being_attacked(king_index, attacker, pieces)
-    attacker.enemy_targets.include?(king_index) &&
+  def self.can_threaten?(king_spaces, attacker, pieces)
+    attacker.valid_moves.any? { |space| king_spaces.include?(space) } &&
       pieces.none? { |piece| piece.enemy_targets.include?(attacker.position_index) }
+  end
+
+  def self.king_spaces(new_pieces, king_index)
+    new_pieces.detect do |piece|
+      piece.position_index == king_index
+    end.spaces_near_king
   end
 end
