38bfc02eacb9f5e251f729c9a64649fb212b27b3
Chad Ellison <chad.ellison0123@gmail.com>
2019-06-01 21:11:25 -0500

    refactor signatures

--- app/models/material_logic.rb
+++ app/models/material_logic.rb
@@ -1,15 +1,17 @@
 class MaterialLogic
   def self.create_signature(new_pieces)
-    all_targets = new_pieces.map(&:enemy_targets).flatten
+    targets = new_pieces.map(&:enemy_targets).flatten
 
     new_pieces.reduce(0) do |total, piece|
-      unless all_targets.include?(piece.position_index)
+      if !targets.include?(piece.position_index)
         piece_value = piece.find_piece_value
         if piece.color == 'white'
           total + piece_value
         else
           total - piece_value
         end
+      else
+        total
       end
     end
   end
