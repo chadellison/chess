9c3cdc7d867b9fae992356ed7b8fd6524f05b013
Chad Ellison <chad.ellison0123@gmail.com>
2019-06-01 21:07:49 -0500

    refactor signatures

--- app/models/check_logic.rb
+++ app/models/check_logic.rb
@@ -1,7 +1,9 @@
 class CheckLogic
   def self.create_signature(new_pieces)
+    targets = new_pieces.map(&:enemy_targets).flatten
+
     new_pieces.reduce(0) do |sum, piece|
-      if can_check?(5, piece, new_pieces)
+      if can_check?(5, piece, new_pieces, targets)
         sum + 1
       elsif can_check?(29, piece, new_pieces)
         sum - 1
@@ -11,8 +13,8 @@ class CheckLogic
     end
   end
 
-  def self.can_check?(king_index, attacker, pieces)
+  def self.can_check?(king_index, attacker, pieces, targets)
     attacker.enemy_targets.include?(king_index) &&
-      pieces.none? { |piece| piece.enemy_targets.include?(attacker.position_index) }
+      !targets.include?(attacker.position_index)
   end
 end
