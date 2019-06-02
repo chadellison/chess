6137f09bcea38b9a6040683b24415a58674dc442
Chad Ellison <chad.ellison0123@gmail.com>
2019-06-01 20:57:05 -0500

    update material signature to exclude targeted pieces

--- app/models/material_logic.rb
+++ app/models/material_logic.rb
@@ -1,12 +1,15 @@
 class MaterialLogic
   def self.create_signature(new_pieces)
-    new_pieces.reduce(0) do |total, piece|
-      piece_value = piece.find_piece_value
+    all_targets = new_pieces.map { |piece| piece.enemy_targets }.flatten
 
-      if piece.color == 'white'
-        total + piece_value
-      else
-        total - piece_value
+    new_pieces.reduce(0) do |total, piece|
+      unless all_targets.include?(piece.position_index)
+        piece_value = piece.find_piece_value
+        if piece.color == 'white'
+          total + piece_value
+        else
+          total - piece_value
+        end
       end
     end
   end
