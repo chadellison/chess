3ebc096a63513a90ece4968d70f72a2ec4cdd8db
Chad Ellison <chad.ellison0123@gmail.com>
2019-08-29 07:45:07 -0500

    wip

--- app/models/neural_network.rb
+++ app/models/neural_network.rb
@@ -1,16 +1,19 @@
 class NeuralNetwork
   ALPHA = 0.1
-  WEIGHT_COUNTS = [24, 18, 3]
-  OFFSETS = [0, 24, 42]
-  VECTOR_COUNTS = [4, 6, 3]
+  WEIGHT_COUNTS = [24, 36, 18]
+  OFFSETS = [0, 24, 60]
+  VECTOR_COUNTS = [4, 6, 6]
 
   include CacheLogic
 
-  attr_reader :layer_one_predictions, :layer_two_predictions,
-    :layer_three_predictions, :layer_one_deltas, :layer_two_deltas,
-    :layer_three_deltas
+  attr_accessor :layer_one_weights, :layer_two_weights, :layer_three_weights,
+    :layer_one_predictions, :layer_two_predictions, :layer_three_predictions,
+    :layer_one_deltas, :layer_two_deltas, :layer_three_deltas
 
   def initialize
+    @layer_one_weights = find_weights(0)
+    @layer_two_weights = find_weights(1)
+    @layer_three_weights = find_weights(2)
     @layer_one_predictions = []
     @layer_two_predictions = []
     @layer_three_predictions = []
@@ -19,17 +22,17 @@ class NeuralNetwork
     @layer_three_deltas = []
   end
 
-  def layer_one_weights
-    @layer_one_weights ||= find_weights(0)
-  end
-
-  def layer_two_weights
-    @layer_two_weights ||= find_weights(1)
-  end
-
-  def layer_three_weights
-    @layer_three_weights ||= find_weights(2)
-  end
+  # def layer_one_weights
+  #   @layer_one_weights ||= find_weights(0)
+  # end
+  #
+  # def layer_two_weights
+  #   @layer_two_weights ||= find_weights(1)
+  # end
+  #
+  # def layer_three_weights
+  #   @layer_three_weights ||= find_weights(2)
+  # end
 
   def move_analysis(possible_moves)
     weighted_moves = {}
@@ -38,10 +41,10 @@ class NeuralNetwork
       initial_input = possible_move.setup.abstraction.pattern.split('.').map do |value|
         normalize_value(value.to_f)
       end
-      final_predictions = calculate_prediction(initial_input)
+      calculate_prediction(initial_input)
 
-      weighted_moves[possible_move.value] = final_predictions
-      puts "#{possible_move.value} ==> #{final_predictions}"
+      weighted_moves[possible_move.value] = layer_three_predictions
+      puts "#{possible_move.value} ==> #{layer_three_predictions}"
     end
     weighted_moves
   end
@@ -50,7 +53,6 @@ class NeuralNetwork
     @layer_one_predictions = leaky_relu(multiply_vector(initial_input, layer_one_weights))
     @layer_two_predictions = leaky_relu(multiply_vector(layer_one_predictions, layer_two_weights))
     @layer_three_predictions = tanh(multiply_vector(layer_two_predictions, layer_three_weights))
-    @layer_three_predictions
   end
 
   def weighted_sum(input, weights)
@@ -132,7 +134,7 @@ class NeuralNetwork
       weight_matrix[index].size.times do |count|
         weight = weight_matrix[index][count]
         adjusted_value = (weight - (ALPHA * weighted_deltas[index][count]))
-        weight = adjusted_value.to_s if adjusted_value > 0
+        weight_matrix[index][count] = adjusted_value
       end
     end
   end
@@ -167,13 +169,12 @@ class NeuralNetwork
       black_wins += setup.outcomes[:black_wins].to_i
       draws += setup.outcomes[:draws].to_i
     end
-
     total_games = white_wins + black_wins + draws
-
-    white_ratio = handle_ratio(white_wins, total_games)
-    black_ratio = handle_ratio(black_wins, total_games)
-    final_ratio = [white_ratio, black_ratio].reduce(0, :+) / 2.0
-    [final_ratio]
+    [
+      handle_ratio(white_wins, total_games),
+      handle_ratio(black_wins, total_games),
+      handle_ratio(draws, total_games),
+    ]
   end
 
   def handle_ratio(numerator, denominator)
