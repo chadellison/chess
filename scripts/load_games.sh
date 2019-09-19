daa7d575c10e5c3615015d9fffffacfd479cad31
selfup <boudinot.regis@yahoo.com>
2019-09-19 12:47:05 -0500

    next step make sperate docker-compose with rake

--- scripts/load_games.sh
+++ scripts/load_games.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-PG_HOST=db
+echo 'sleeping for 20 seconds'
 
-rake db:create;
-rake db:migrate;
-rake load_chess_games;
+/bin/sleep 20
+
+echo 'performing operations'
+
+PG_HOST=db rake db:create;
+PG_HOST=db rake db:migrate;
+PG_HOST=db rake load_chess_games;
+
