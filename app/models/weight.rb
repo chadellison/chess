96cb42cf3c4c1bad5386391bf6a6a2507fb7469f
Chad Ellison <chad.ellison0123@gmail.com>
2019-09-25 17:09:50 -0500

    expirement with weight adjustment

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -3,7 +3,7 @@ class Weight < ApplicationRecord
     Weight.destroy_all
 
     660.times do |count|
-      Weight.create(weight_count: count + 1, value: rand)
+      Weight.create(weight_count: count + 1, value: (rand * 0.1))
     end
   end
 end
