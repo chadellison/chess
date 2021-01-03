3ca918521e78e45512c127403b37dc4961f2ef47
Chad Ellison <chad.ellison0123@gmail.com>
2021-01-03 12:27:38 -0700

    add tempo abstraction

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -3,24 +3,42 @@ class Attack
     -AbstractionHelper.max_target_value(pieces) * 0.1
   end
 
-  def self.create_attack_abstraction(position_data)
-    targets = find_targets(position_data.pieces)
-    max_vulnerability = AbstractionHelper.max_value(targets, 0)
+  def self.tempo_abstraction(position_data)
+    value = 0
 
-    attack_value = 0
-    position_data.next_pieces.each do |attacker|
-      if targets.none? { |target| target.position == attacker.position }
-        max = AbstractionHelper.max_value(attacker.targets, 0)
-        value = max - max_vulnerability
-        attack_value = value if value > attack_value
+    position_data.next_pieces.each do |piece|
+      if !position_data.target_positions.include?(piece.position)
+        piece_value = AbstractionHelper.find_piece_value(piece)
+        max_opponent_value = AbstractionHelper.max_value(piece.targets, 0)
+        difference = max_opponent_value - piece_value
+        value = difference if difference > value
       end
     end
-    attack_value
+    value
   end
 
-  def self.find_targets(pieces)
-    pieces.reduce([]) do |accumulator, piece|
-      accumulator + piece.targets
-    end
+  def self.undefended_abstraction(position_data)
+    # max attack of undefended pieces
   end
+
+  # def self.create_attack_abstraction(position_data)
+  #   targets = find_targets(position_data.pieces)
+  #   max_vulnerability = AbstractionHelper.max_value(targets, 0)
+  #
+  #   attack_value = 0
+  #   position_data.next_pieces.each do |attacker|
+  #     if targets.none? { |target| target.position == attacker.position }
+  #       max = AbstractionHelper.max_value(attacker.targets, 0)
+  #       value = max - max_vulnerability
+  #       attack_value = value if value > attack_value
+  #     end
+  #   end
+  #   attack_value
+  # end
+
+  # def self.find_targets(pieces)
+  #   pieces.reduce([]) do |accumulator, piece|
+  #     accumulator + piece.targets
+  #   end
+  # end
 end
